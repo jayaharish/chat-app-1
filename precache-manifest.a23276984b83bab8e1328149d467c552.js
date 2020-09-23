46082819c04e50813133451afb0787e73d4d10fe
jayaharish <17tume051@skct.edu.in>
2020-09-23 16:42:06 +0530

    Updates

---
+++ precache-manifest.a23276984b83bab8e1328149d467c552.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3d78e9837d872570cd961a42b8f061d",
+    "url": "/chat-app-1/index.html"
+  },
+  {
+    "revision": "890d1efd01b6cebb7bb0",
+    "url": "/chat-app-1/static/css/main.78068f99.chunk.css"
+  },
+  {
+    "revision": "3130670e2cd9e849a745",
+    "url": "/chat-app-1/static/js/2.ddec4a1b.chunk.js"
+  },
+  {
+    "revision": "d0550c944a9928585a31b6efc1a5d942",
+    "url": "/chat-app-1/static/js/2.ddec4a1b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "890d1efd01b6cebb7bb0",
+    "url": "/chat-app-1/static/js/main.01983a50.chunk.js"
+  },
+  {
+    "revision": "c5e14c235aafe3b5315d",
+    "url": "/chat-app-1/static/js/runtime-main.f63e5a9e.js"
+  },
+  {
+    "revision": "0929ca7ac76149368a7d112bbe8e09a9",
+    "url": "/chat-app-1/static/media/back.0929ca7a.png"
+  }
+]);
