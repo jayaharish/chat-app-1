3f8adff6c9067c29f344760a4d4dbd1c513f74cb
Suresh <asuresh52012@gmail.com>
2020-09-23 15:49:56 +0530

    Add files via upload

---
+++ build/precache-manifest.33d46e93795d1131d38c528a3d0dc0fb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "440c18772b793341105eca116338b0d9",
+    "url": "/chat-app/index.html"
+  },
+  {
+    "revision": "1accbcd30e2ab0204c68",
+    "url": "/chat-app/static/css/main.874bc41b.chunk.css"
+  },
+  {
+    "revision": "3b7fd198daa386b535ad",
+    "url": "/chat-app/static/js/2.ca8ac912.chunk.js"
+  },
+  {
+    "revision": "d0550c944a9928585a31b6efc1a5d942",
+    "url": "/chat-app/static/js/2.ca8ac912.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1accbcd30e2ab0204c68",
+    "url": "/chat-app/static/js/main.bd2ef5f4.chunk.js"
+  },
+  {
+    "revision": "844d941a2013a08fe2e0",
+    "url": "/chat-app/static/js/runtime-main.bdb3b2b2.js"
+  },
+  {
+    "revision": "0929ca7ac76149368a7d112bbe8e09a9",
+    "url": "/chat-app/static/media/back.0929ca7a.png"
+  }
+]);
